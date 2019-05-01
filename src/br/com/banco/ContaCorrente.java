bc9b2cd0910f188dcc1ebb376a44749195b89c0b
anavollu <anavollu@id.uff.br>
2019-05-01 16:29:45 -0300

    Tributo conta corrente

---
+++ src/br/com/banco/ContaCorrente.java
@@ -0,0 +1,35 @@
+package br.com.banco;
+
+/**
+ * Conta
+ */
+public class ContaCorrente extends Conta implements Tributavel{
+
+    private Cliente titular;
+
+    public ContaCorrente(Cliente titular, int numero, String agencia, String dataAbertura) {
+        super(titular, numero, agencia, dataAbertura);
+    }    
+
+    public double calcularRendimento(int periodo) {
+        return (Math.pow(1.1, periodo) - 1);
+    }
+
+    public double calcularTributacao(int periodo) {
+        return calcularRendimento(periodo) * 0.15;
+    }
+
+    public String getTipo() {
+        return "Conta Corrente";
+    }
+
+    public String getTitular() {
+        return this.titular.toString();
+    }
+
+    //Para a Conta Corrente, você precisa pagar 15% ao ano sobre o saldo
+    public double getValorImposto() {
+        return super.getSaldo() * 0.15;
+    }
+
+}
