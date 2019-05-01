c89a7e4bf9d38a28a176d2f39a03b8d9462dc364
anavollu <anavollu@id.uff.br>
2019-05-01 17:03:27 -0300

    atualizaçoes main

--- src/app/App.java
+++ src/app/App.java
@@ -7,15 +7,25 @@ public class App {
 
         Cliente cliente1 = new Cliente("Flavio", "Seixas", "123456");
 
-        Conta conta1 = new ContaCorrente(cliente1, 1, "UFF", "03/04/2019");
-        Conta conta2 = new ContaPoupanca(cliente1, 1, "UFF", "03/04/2019");
+        ContaCorrente contaCorrente = new ContaCorrente(cliente1, 1, "UFF", "03/04/2019");
+        ContaPoupanca contaPoupanca = new ContaPoupanca(cliente1, 1, "UFF", "03/04/2019");
+        SeguroDeVida seguroDeVida = new SeguroDeVida(cliente1, 1, 1000);
         
-        conta1.depositar(1000);
-        conta2.depositar(1000);
+        contaCorrente.depositar(1000);
+        contaPoupanca.depositar(1000);
+
+        Tributavel tributoContaCorrente = contaCorrente1;
+        Tributavel tributoSeguroDeVida = seguroDeVida;
+
+        tributoContaCorrente.getValorImposto();
+        tributoContaCorrente.getTipo();
+
+        tributoSeguroDeVida.getValorImposto();
+        tributoSeguroDeVida.getTipo();
+
+        cliente1.ativos(contaCorrente, contaPoupanca, seguroDeVida);
 
         System.out.println("Saldo futuro para conta corrente: " + conta1.calcularSaldoFuturo(5));
         System.out.println("Saldo futuro para conta poupanca: " + conta2.calcularSaldoFuturo(5));
-
-
     }
 }
