893fe7211e195fd13390164bb412d28a6bfba7bb
anavollu <anavollu@id.uff.br>
2019-05-01 16:44:40 -0300

    ativos

--- src/br/com/banco/Cliente.java
+++ src/br/com/banco/Cliente.java
@@ -3,7 +3,7 @@ package br.com.banco;
 /**
  * Cliente
  */
-public class Cliente {
+public class Cliente implements Ativos {
 
     private String primeiroNome;
     private String ultimoNome;
@@ -13,5 +13,25 @@ public class Cliente {
         this.primeiroNome = primeiroNome;
         this.ultimoNome = ultimoNome;
         this.cpf = cpf;
+    }
+
+    public String toString() {
+        return "Nome: " + this.primeiroNome + " " + this.ultimoNome + " / CPF: " + this.cpf;
+    }
+
+    public void ativos(ContaCorrente contaC, ContaPoupanca contaP, SeguroDeVida seguroV) {
+        Object[] ativos = new Object[3];
+        ativos[0] = contaC;
+        ativos[1] = contaP;
+        ativos[2] = seguroV;
+
+        for (Object ativo:ativos) {
+            if (ativo instanceof Tributavel) {
+                System.out.println(((Tributavel) ativo).getTipo() + ": Imposto devido: " + ((Tributavel) ativo).getValorImposto());
+            } else {
+                System.out.println(((Conta) ativo).getTipo() + ": Ativo não tributável.");
+            }
+        }
+        return;
     }
 }
